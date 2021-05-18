33ca7e4814621104ebf7f7731043bb7ae7bfc4ee
eprusty <epsita.prusty@gmail.com>
2021-05-17 21:43:30 -0400

    updated the constant.java file

--- src/main/java/com/dd/util/Constants.java
+++ src/main/java/com/dd/util/Constants.java
@@ -2,70 +2,9 @@ package com.dd.util;
 
 public class Constants {
 
-	// Developement Envirement
-//	public static String BASE_URL = "https://chrmtl-chmut-dt1.pegacloud.net/prweb";
-//	public static String ACCESS_TOKEN_BASE_URL = "https://chrmtl-chmut-dt1.pegacloud.net/prweb/PRRestService";
-//
-//	public static String OAUTH_CLIENT_ID = "21968530703152351903";
-//	public static String OAUTH_CLIENT_SECRET = "69E1D24116D181325BA2AE57E2DFA68D";
-
 	public static String BASE_URL = "https://www.dollardays.com";
 
-	//public static String BASE_URL = "https://chrmtl-chmut-dt1.pegacloud.net/prweb";
-	public static String ACCESS_TOKEN_BASE_URL = "https://chrmtl-chmut-stg1.pegacloud.net/prweb/PRRestService";
-
-	public static String OAUTH_CLIENT_ID = "39788970635915082042";
-	public static String OAUTH_CLIENT_SECRET = "E20B330177208A4EDAACD0844E0AAA54";
-
-	public static String PEGA_API_ENDPOINT_URL = BASE_URL + "/api/v1";
-	public static String ATFSERVICE_LOCAL_ACTION_ENDPOINT_URL = BASE_URL + "/PRRestService/ATFServices/v2";
-
-	public static String ATFSERVICE_UPDATE_RESUME_ENDPOINT_URL = BASE_URL + "/PRRestService/ATFServices/v1";
-
-	public static String CREATE_RENEWAL_CASE_URL = BASE_URL + "/PRRestService/ams/v1";
-
-	public static String PORT_NUMBER = "587";
-
-	public static String api_passWord = System.getenv("Api_PassWord");
-
-	public static String BASIC_AUTH_USERNAME = "AMServiceUserAPI";
-	public static String BASIC_AUTH_PASSWORD = "rules";
-
-	public static String BASIC_PASSWORD = "rules";
-
+	
 	public static boolean IS_ENABLE_PROXY = false;
-
-	public static String SalesRepresentative = "CM001511";// DougLeclair
-
-	public static String UW_CM001511 = "CM001511";
-	
-	public static String R_MEEHAN = "RMeehan@cmic";
-
-	public static String ClearnsTeam = "megshortridge@cmic";
-
-	public static String CLICK_CONVERTLEAD = "Convert lead";
-
-	public static String AddressVerification_ConvertLead = "Address Verification completed.Click here to validate";
-
-	public static String UW_REVIEW = "Underwriter Review";
-	
-	public static String REFRESh = "Refresh";
-	
-	public static String SUBMIT = "Submit";
-	
-	public static String CONTACT_ASSERTION = "Contacts";
-	
-	public static String CLICK_ON_CREATECONTACT = "Create contact";
-	
-	public static String BETH_LOGIN = "beth@cmic";
-	
-	public static String MEGSHORTRIDGE = "megshortridge@cmic";
-	
-	public static String RICK_LOGIN = "Rick";
-	
-	public static String UW_APPROVE= "Approve";
-	
-	public static String SUBMIT_TO_PRODUCER= "Submit To Producer";
-
 }
 
