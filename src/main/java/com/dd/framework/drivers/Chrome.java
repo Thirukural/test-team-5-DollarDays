a3d05bf2ddc26f7d3eef42e558e239919fecf929
eprusty <epsita.prusty@gmail.com>
2021-06-11 22:03:45 -0400

    updated the chromedriver name for mac

--- src/main/java/com/dd/framework/drivers/Chrome.java
+++ src/main/java/com/dd/framework/drivers/Chrome.java
@@ -28,7 +28,7 @@ public class Chrome implements Browsers {
     //		System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver-64-linux");
     	System.out.println("operatingSystem----->"+operatingSystem);
     	if(operatingSystem.equalsIgnoreCase("Mac OS X"))
-    		System.setProperty("webdriver.chrome.driver","src//main//resources//chromedriver");
+    		System.setProperty("webdriver.chrome.driver","src//main//resources//chromedriver-64-mac");
     	else if (operatingSystem.equalsIgnoreCase("Linux")) {
     		System.out.println("Executing in Linux OS .......");
     		System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver-64-linux");
@@ -84,7 +84,7 @@ public class Chrome implements Browsers {
             	//if(operatingSystem.equalsIgnoreCase("linux"))
             		//driverPath = "src/main/resources/chromedriver-64-linux";
             	if(operatingSystem.equalsIgnoreCase("Mac OS X"))
-            		driverPath = "src//main//resources//chromedriver";
+            		driverPath = "src//main//resources//chromedriver-64-mac";
             	else if(operatingSystem.equalsIgnoreCase("Linux")){
             		System.out.println("before 2222");
             		driverPath = "src/main/resources/chromedriver-64-linux";  
